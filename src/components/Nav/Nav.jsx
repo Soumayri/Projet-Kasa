7db51204c42607960b96b0ddfb9f5c61ff0318f1
soumayri <studsoum@gmail.com>
2024-08-14 07:31:50 +0200

    change-style-nav__logo-home-link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,26 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link, useLocation } from "react-router-dom";
 
 export default function Nav() {
-	return (
-		<nav className="nav-header">
-			<Link to="/" className="nav-header_link-home">
-				Accueil
-			</Link>
-			<Link to="/about" className="nav-header_link-about">
-				A Propos
-			</Link>
-		</nav>
-	);
+  const location = useLocation();
+
+  // Vérifiez si l'utilisateur est sur la page "À Propos"
+  const isAboutPage = location.pathname === '/about';
+
+  return (
+    <nav className="nav-header">
+      <Link
+        to="/"
+        className={`nav-header_link-home ${isAboutPage ? 'nav-link-black' : ''}`}
+      >
+        Accueil
+      </Link>
+      <Link
+        to="/about"
+        className={`nav-header_link-about ${isAboutPage ? 'nav-link-black' : ''}`}
+      >
+        A Propos
+      </Link>
+    </nav>
+  );
 }
