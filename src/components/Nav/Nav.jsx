efb30696b0e8c998ce43430495b54d57050d2d40
soumayri <studsoum@gmail.com>
2024-08-17 12:57:38 +0200

    correction couleur liens de navigation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
-  const location = useLocation();
 
-  // Vérifiez si l'utilisateur est sur la page "À Propos"
-  const isAboutPage = location.pathname === '/about';
 
   return (
     <nav className="nav-header">
       <Link
         to="/"
-        className={`nav-header_link-home ${isAboutPage ? 'nav-link-black' : ''}`}
+        className="nav-header_link-home"
       >
         Accueil
       </Link>
       <Link
         to="/about"
-        className={`nav-header_link-about ${isAboutPage ? 'nav-link-black' : ''}`}
+        className="nav-header_link-about"
       >
         A Propos
       </Link>
